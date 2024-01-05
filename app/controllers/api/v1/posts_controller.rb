ff79eae55d856ec89064e94c187310dd4afadeb9
Jihun Choi <workbyatlas@gmail.com>
2024-01-04 21:50:56 -0500

    Add routing & post show page

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -10,6 +10,7 @@ class Api::V1::PostsController < ApplicationController
 
   # GET /posts/1
   def show
+    sleep 3 
     render json: @post
   end
 
---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+//Posts List Link (Root Path) | Link to Crete New Post (Post Form)
+import React from "react";
+import { Link } from "react-router-dom";
+
+function NavBar(){
+	return(
+		<nav>
+		<Link to="/">Post List</Link>
+		{" | "}
+		<Link to="/new">New Post</Link>
+		</nav>
+		);
+		
+	}
+
+	export default NavBar;
+
