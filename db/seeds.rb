42a8490b2ac54878ff5e5dac9fd06ad98d9de05e
Jihun Choi <workbyatlas@gmail.com>
2024-01-04 20:05:29 -0500

    init commit

---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+
+20.times do |i|
+	Post.create(
+		title:"Post #{i}",
+		body: "this is a body."
+		)
+end
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
