9474e09ae835421879311017cf72b4c3b2d09ee6
Jihun Choi <workbyatlas@gmail.com>
2024-01-05 19:09:59 -0500

    Added Edit & Delete

--- client/src/components/AppRoutes.jsx
+++ client/src/components/AppRoutes.jsx
@@ -4,12 +4,14 @@ import { Route, Routes } from "react-router-dom";
 import PostsList from "../features/posts/PostsList";
 import NewPostForm from "../features/posts/NewPostForm";
 import PostDetails from "../features/posts/PostDetails";
+import EditPostForm from "../features/posts/EditPostForm";
 
 function AppRoutes(){
 	return(
 		<Routes>
 			<Route path="/"         element={<PostsList   />} />
 			<Route path="posts/:id" element={<PostDetails />} />
+			<Route path="posts/:id/edit" element={<EditPostForm />} />
 			<Route path="/new"      element={<NewPostForm />} />
 		</Routes>
 		);
--- client/src/features/posts/PostDetails.jsx
+++ client/src/features/posts/PostDetails.jsx
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from "react";
 import { useParams, useNavigate, Link } from "react-router-dom";
 import { API_URL } from "../../constants";
 
+
 function PostDetails() {
   const [post, setPost] = useState(null);
   const { id } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchCurrentPost = async () => {
@@ -23,13 +25,32 @@ function PostDetails() {
     fetchCurrentPost();
   }, [id]);
 
+    const deletePost = async () => {
+    try {
+      const response = await fetch(`${API_URL}/${id}`, {
+        method: "DELETE",
+      });
+      if (response.ok) {
+        navigate("/");
+      } else {
+        throw response;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   if (!post) return <h2>Loading...</h2>;
 
   return (
     <div>
       <h2>{post.title}</h2>
       <p>{post.body}</p>
+      <Link to={`/posts/${post.id}/edit`}>Edit</Link>
+      {" | "}
       <Link to="/">Back to Posts</Link>
+      {" | "}
+      <button onClick={deletePost}>Delete</button>
     </div>
   );
 }
